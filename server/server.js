16631ed4c55b490e5a481fee03ad5b9166066578
Tanmay Baweja <tanmaybaweja1999@gmail.com>
2018-07-05 01:10:00 +0530

    first commit for chatApp

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+	console.log(`server starting at ${port}`);
+});
