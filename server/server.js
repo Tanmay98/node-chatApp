1801bef04038f2d881f77f75fa340eab6d577277
Tanmay Baweja <tanmaybaweja1999@gmail.com>
2018-07-07 18:03:55 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-app = express();
+var app = express();
+var server = http.createServer(app)
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.on('disconnect', () => {
+		console.log('Connection lost');
+	});
+});
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
 	console.log(`server starting at ${port}`);
 });
