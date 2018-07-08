a73830447d8bc42b16857c95d3a953dec4235dcb
Tanmay Baweja <tanmaybaweja1999@gmail.com>
2018-07-08 19:35:35 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the group',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New Member joined',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('new Message', message);
 		io.emit('newMessage', {
